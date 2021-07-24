0d6d129c03a19afabef9117991eb57cf4303ccee
hetfly <hetfly@gmail.com>
2021-07-24 11:49:26 +0200

    feat: add episode frontmatter

--- src/pages/eye-on-icon/index.js
+++ src/pages/eye-on-icon/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+import EpisodesRoll from '../../components/EpisodesRoll'
 
 export default class EyeOnIconIndexPage extends React.Component {
   render() {
@@ -27,7 +28,7 @@ export default class EyeOnIconIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              episodes list
+              <EpisodesRoll />
             </div>
           </div>
         </section>
