8ef93cdbea330010dbbdcf651885fe215744a335
hetfly <hetfly@gmail.com>
2021-09-26 12:02:08 +0200

    add: favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,30 +17,19 @@ const TemplateWrapper = ({ children }) => {
         {/* <title>{title}</title> */}
         <title>Eye on ICON</title>
         <meta name="description" content='Latest stories from ICON ecosystem.' />
-
-        {/* <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        /> */}
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
-
-        {/* <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        /> */}
+        <link rel="apple-touch-icon" sizes="57x57" href={`${withPrefix('/')}favicon/apple-icon-57x57.png`} />
+        <link rel="apple-touch-icon" sizes="60x60" href={`${withPrefix('/')}favicon/apple-icon-60x60.png`} />
+        <link rel="apple-touch-icon" sizes="72x72" href={`${withPrefix('/')}favicon/apple-icon-72x72.png`} />
+        <link rel="apple-touch-icon" sizes="76x76" href={`${withPrefix('/')}favicon/apple-icon-76x76.png`} />
+        <link rel="apple-touch-icon" sizes="114x114" href={`${withPrefix('/')}favicon/apple-icon-114x114.png`} />
+        <link rel="apple-touch-icon" sizes="120x120" href={`${withPrefix('/')}favicon/apple-icon-120x120.png`} />
+        <link rel="apple-touch-icon" sizes="144x144" href={`${withPrefix('/')}favicon/apple-icon-144x144.png`} />
+        <link rel="apple-touch-icon" sizes="152x152" href={`${withPrefix('/')}favicon/apple-icon-152x152.png`} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${withPrefix('/')}favicon/apple-icon-180x180.png`} />
+        <link rel="icon" type="image/png" sizes="192x192"  href={`${withPrefix('/')}favicon/android-icon-192x192.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${withPrefix('/')}favicon/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="96x96" href={`${withPrefix('/')}favicon/favicon-96x96.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${withPrefix('/')}favicon/favicon-16x16.png`} />
         <meta name="theme-color" content="#171717" />
 
         {/* <meta property="og:title" content={title} /> */}
